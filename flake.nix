e6d7fefc0c46049eb84dadfc1a1a629c65b6338d
DavHau <hsngrmpf+github@gmail.com>
2023-02-11 17:16:17 +0700

    init

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  description = "";
+
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-unstable";
+    flake-compat = {
+      url = "github:edolstra/flake-compat";
+      flake = false;
+    };
+  };
+
+  outputs = inputs@{ flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+
+      systems = [
+        "aarch64-darwin"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "x86_64-linux"
+      ];
+
+      imports = [
+        ./nix/modules/flake-parts/modules.nix
+      ];
+    };
+}
